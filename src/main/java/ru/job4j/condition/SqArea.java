c0d016784e07505002680bd0ff16e7e9539a1594
Ruslan-Popov <ruslan_popov@mail.ru>
2023-02-26 01:49:13 +0300

    1.1.2.8. Стороны прямоугольника. [#137735 #31714]

--- src/main/java/ru/job4j/condition/SqArea.java
+++ src/main/java/ru/job4j/condition/SqArea.java
@@ -31,5 +31,6 @@ public class SqArea {
         System.out.println("p = 6, k = 2, s = 2, real = " + result2);
         double result3 = square(30, 4);
         System.out.println("p = 30, k = 4, s = 36, real = " + result3);
+        //check
     }
 }
