3e50aa5d0d0bd83b14307463794db3cbe34d0b47
Popov-Ruslan <ruslan.popov87@gmail.com>
2023-02-28 01:20:02 +0300

    1.1.3.0. Что такое тестирование [#159431 #31669]

---
+++ src/main/java/ru/job4j/Symbols.java
@@ -0,0 +1,19 @@
+package ru.job4j;
+
+public class Symbols {
+    public static void main(String[] args) {
+        char c1 = '\u0035';
+        char c2 = '\u004D';
+        char c3 = '\u006E';
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+        System.out.println("\u0031\u0032\u0033");
+        char a = 'A';
+        char b = 'B';
+        System.out.println(a + b);
+        System.out.println(++a);
+        char d = 'D';
+        System.out.println(--d);
+    }
+}
--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -2,19 +2,23 @@ package ru.job4j.converter;
 
 public class Converter {
     public static float rubleToEuro(float value) {
-        float result = value / 80;
-        return result;
+        return  value / 80;
     }
 
     public static float rubleToDollar(float value) {
-        float result = value / 75;
-        return result;
+        return value / 75;
     }
 
     public static void main(String[] args) {
-        float euro = rubleToEuro(160);
-        System.out.println("160 rubles are " + euro + " euros");
-        float dollar = rubleToDollar(225);
-        System.out.println("225 rubles are " + dollar + " dollars");
+        float in = 160;
+        float expected = 2;
+        float euro = rubleToEuro(in);
+        boolean passed = expected == euro;
+        System.out.println("160 rubles to euro are 2. Test result: " + passed);
+        float inDol = 225;
+        float expected2 = 3;
+        float dollar = rubleToDollar(inDol);
+        boolean passed2 = expected2 == dollar;
+        System.out.println("225 rubles to dollar are 3. Test result: " + passed2);
     }
 }
