7d78debab8f224b634e8a8e4a866111183b44e61
Popov-Ruslan <ruslan.popov87@gmail.com>
2023-03-10 00:00:29 +0300

    1.1.6.6.0.3. Заполнение массива. [#226300 #31700]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,13 @@ public class ArrayDefinition {
         System.out.println(surnames.length);
         float[] prices = new float[40];
         System.out.println(prices.length);
+        String[] names = new String[4];
+        names[0] = "Сергей";
+        names[1] = "Артем";
+        names[2] = "Матвей";
+        names[3] = "Арсений";
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
     }
 }
